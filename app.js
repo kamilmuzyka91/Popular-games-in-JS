e6876951359dd23650f4726567cd3bfc944d4a00
Kamil <kamil.muzyka91@gmail.com>
2022-03-05 18:56:17 +0100

    added random logic

--- app.js
+++ app.js
@@ -7,18 +7,30 @@ document.addEventListener("DOMContentLoaded", () => {
   const possibleChoices = document.querySelectorAll("button");
 
   let userChoice;
+  let computerChoice;
 
   possibleChoices.forEach((possibleChoices) =>
     possibleChoices.addEventListener("click", (e) => {
       userChoice = e.target.id;
-      userChoiceDisplay.innerText = userChoice;
+      userChoiceDisplay.innerHTML = userChoice;
 
-      generateComputerChoiseDisplay();
+      generateComputerChoiceDisplay();
     })
   );
 
-  function generateComputerChoiseDisplay() {
+  function generateComputerChoiceDisplay() {
     const randomNumber = Math.floor(Math.random() * possibleChoices.length + 1); // form 0 to 2 + 1 never give 0
-    console.log(randomNumber);
+
+    if (randomNumber === 1) {
+      computerChoice = "rock";
+    }
+    if (randomNumber === 2) {
+      computerChoice = "paper";
+    }
+    if (randomNumber === 3) {
+      computerChoice = "scissors";
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice;
   }
 });
